d79f9f19093cd851f5f30a5e06b62b423c2fcdb6
Justin Green <justingreen@Justins-MacBook-Air.local>
2023-10-26 19:13:20 -0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,30 @@ function App() {
   const [showBottom,setShowBottom] = useState(false)
   const [isDay,setIsDay] = useState(true);
   const [quote,setQuote] = useState({author:"John Doe", quote:"It is far greater to have f-ed something up, then to have never even tried to begin with...sometimes at least, probably 🤔"})
+  const [hour,setHour] = useState(new Date(Date.now()).toLocaleTimeString().split(":")[0])
+  const [minute,setMinute] = useState(new Date(Date.now()).toLocaleTimeString().split(":")[1])
+  const dayOfWeek = new Date().getDay();
+  const dayOfYear = Math.floor((new Date() - new Date(new Date().getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+  const weekNumber = Math.ceil(dayOfYear / 7);
   const appRef = useRef();
   
+  const quoteURL = `https://api.quotable.io/random`
+
 
 
 
   useEffect(()=>{
+    // fetchQuote();
+    determineIfDay();
+    // if(isDay != null){
+    //   console.log("IsDay",isDay)
+    setBg();
+    console.log(new Date(Date.now()).toLocaleTimeString());
+    // }
+  },[isDay]);
 
-    setBg(isDay)
-  },[]);
 
-
-  const setBg =(isDay)=>{
+  const setBg =()=>{
       if(isDay){
         appRef.current.style.backgroundImage = `url(${bgImageDaytime})`
       }
@@ -26,6 +38,58 @@ function App() {
 
       }
   }
+
+  const determineIfDay = ()=>{
+    let hours = new Date().getHours();
+    console.log("HOURS",hours)
+
+    if(hours > 7 && hours < 18)setIsDay(true)
+    else {
+      console.log("nightime!!");
+      setIsDay(false);
+    }
+
+  }
+
+  onresize=(e)=>{
+    console.log(innerWidth)
+    if(innerWidth > 1050){
+      if(isDay){
+        appRef.current.style.backgroundImage = `url(${bgImageDaytime})`
+        }
+      else{
+        appRef.current.style.backgroundImage = `url(${bgImageNighttime})`
+
+      }
+    }
+    else if(innerWidth > 550){
+      if(isDay){
+        appRef.current.style.backgroundImage = `url(${bgTabletImageDaytime})`
+        }
+      else{
+        appRef.current.style.backgroundImage = `url(${bgTabletImageNighttime})`
+
+      }
+    }
+    else{
+      if(isDay){
+        appRef.current.style.backgroundImage = `url(${bgMobileImageDaytime})`
+        }
+      else{
+        appRef.current.style.backgroundImage = `url(${bgMobileImageNighttime})`
+
+      }
+
+    }
+  }
+
+  const fetchQuote = async()=>{
+    let response = await fetch(quoteURL);
+    response = await response.json();
+    console.log(response)
+    setQuote({author:response.author,quote:response.content})
+  }
+
 
   return (
     <div ref={appRef} className="app">
@@ -41,7 +105,7 @@ function App() {
             </div>
           </div>
         </div>
-        <div className="center-app-row main-row">
+        <div className={showBottom ? "center-app-row main-row elevate-center" : "center-app-row"}>
           <div className="main-row-column">
             <div className="main-row-column-content">
               <div className="main-row-content-header">
@@ -51,9 +115,9 @@ function App() {
                 <h4 className="uppercase thin">Good morning, it's currently</h4>
               </div>
               <div className="time-content">
-                <h1>11:37</h1>
+                <h1>{hour}:{minute}</h1>
                 {/* <div> */}
-                <h3 className="timezone">BST</h3>
+                <h3 className="timezone thin">BST</h3>
                 {/* </div> */}
               </div>
               <div className="location-btn-content">
@@ -61,7 +125,7 @@ function App() {
                 <button className="btn" onClick={()=>setShowBottom(!showBottom)} >
                   <div className="btn-content">
                     <p className="thin uppercase">{showBottom ? "Less" : "More"}</p>
-                    <img src={arrowUpIcon} alt="arrow" />
+                    <img className={showBottom ? "arrow-icon rotate-arrow" : "arrow-icon"} src={arrowUpIcon} alt="arrow" />
                   </div>
                 </button>
               </div>
@@ -70,7 +134,7 @@ function App() {
         </div>
         <div className={showBottom ?"bottom-app-row info-row" : "bottom-app-row info-row hide-bottom-row"}>
           <div className="bottom-overlay"></div>
-          <div className={isDay ? "bottom-grid-content" : "bottom-grid-content dark-text"}>
+          <div className={isDay ? "bottom-grid-content dark-text" : "bottom-grid-content"}>
             <div className="bottom-grid">
               <div className="grid-container">
                 <small className="small thin">Current Timezone</small>
@@ -78,15 +142,15 @@ function App() {
               </div>
               <div className="grid-container">
                 <small className="small thin">Days of the Week</small>
-                <h2>5</h2>
+                <h2>{dayOfWeek}</h2>
               </div>
               <div className="grid-container">
                 <small className="small thin">Days of the Year</small>
-                <h2>295</h2>
+                <h2>{dayOfYear}</h2>
               </div>
               <div className="grid-container">
                 <small className="small thin">Week number</small>
-                <h2>42</h2>
+                <h2>{weekNumber}</h2>
               </div>
             </div>
           </div>
